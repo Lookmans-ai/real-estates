a44aa614195d6a7bfc8f3391f0596f13cc4f0b9e
lookmans.ai <Lookmans.007@gmail.com>
2025-08-21 23:56:51 +0200

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
-// import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 import bcryptjs from 'bcryptjs';
 
 export const signup = async (req, res, next) => {
@@ -33,6 +33,38 @@ export const signup = async (req, res, next) => {
     await newUser.save();
 
     return res.status(201).json({ message: 'User created successfully!' });
+  } catch (error) {
+    if (error.code === 11000) {
+      next(errorHandler(400, 'Wrong credentials!'));
+    } else {
+      next(error);
+    }
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    // return res.status(400).json({ message: 'Please fill all input!' });
+    return next(errorHandler(400, 'Please fill in all input!'));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, 'User not found!'));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .cookie('access_token', token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     if (error.code === 11000) {
       next(errorHandler(400, 'Wrong credentials!'));
