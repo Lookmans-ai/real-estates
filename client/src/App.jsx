0fbf8ce389ab1150e09f198ead2b0ed7c77100ef
lookmans.ai <Lookmans.007@gmail.com>
2025-08-21 13:12:25 +0200

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 export default function App() {
   return (
-    <h1 className='text-3xl text-red-500 font-bold underline'>Hello world!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/sign-in' element={<Signin />}></Route>
+        <Route path='/sign-up' element={<Signup />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/profile' element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
