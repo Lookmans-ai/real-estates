cd8f691d99a05c7d3594e8d7bf96db5bcd51c2ad
lookmans.ai <Lookmans.007@gmail.com>
2025-09-05 01:09:33 +0200

    create get user listings api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,6 +3,7 @@ import {
   deleteUser,
   test,
   updateUser,
+  getUserListings,
 } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
@@ -11,5 +12,6 @@ const userRouter = express.Router();
 userRouter.get('/test', test);
 userRouter.put('/update/:id', verifyToken, updateUser);
 userRouter.delete('/delete/:id', verifyToken, deleteUser);
+userRouter.delete('/listing/:id', verifyToken, getUserListings);
 
 export default userRouter;
