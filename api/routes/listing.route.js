0da990d5879c75abd2be9559ff9581b8f825ea86
lookmans.ai <Lookmans.007@gmail.com>
2025-09-15 23:34:36 +0200

    update create api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {
   createListing,
   deleteListing,
+  updateListing,
 } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
@@ -9,5 +10,5 @@ const listingRouter = express.Router();
 
 listingRouter.post('/create', verifyToken, createListing);
 listingRouter.delete('/delete/:id', verifyToken, deleteListing);
-
+listingRouter.put('/update/:id', verifyToken, updateListing);
 export default listingRouter;
